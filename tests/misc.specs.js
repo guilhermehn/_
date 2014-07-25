95944b19c0d7c5245499ab11236cf9b79a6df7ca
Guilherme Nagatomo <gnagotomo@sp.r7.com>
2014-07-25 19:22:57 -0300

    add more test cases and expect.js

---
+++ tests/misc.specs.js
@@ -0,0 +1,51 @@
+var expect = require('expect.js')
+  , _ = require('../')
+
+describe('_.filter() use cases', function () {
+  var filterStrings = _.filter(_.isString)
+    , onlyMajorAge = _.filter(isMajor)
+    , persons = [
+        { name: 'John Doe', age: 20 }
+      , { name: 'Another', age: 17 }
+      , { name: 'Children', age: 5 }
+      , { name: 'Kid', age: 10 }
+      , { name: 'Elder', age: 50 }
+    ]
+
+  function isMajor (person) {
+    return person.age > 18
+  }
+
+  describe('filterStrings', function () {
+    var list = [1, '1', '2', 2, '3', 3]
+      , list2 = [1, 2, 3]
+    it('should filter strings from the list', function () {
+      expect(filterStrings(list)).to.eql([1, 2, 3])
+      expect(filterStrings(list2)).to.eql([])
+    })
+
+    it('should work with empty lists', function () {
+      expect(filterStrings([])).to.eql([])
+    })
+  })
+
+  describe('onlyMajorAge', function () {
+    it('should filter collection for persons of age over 18', function () {
+
+      expect(onlyMajorAge(persons).length).to.be(2)
+    })
+
+    it('should work with empty collection', function () {
+      expect(onlyMajorAge([])).to.eql([])
+    })
+
+    it('should work even if person dont have age prop', function () {
+      var persons = [
+          { name: 'John Doe', age: 20 }
+        , { name: 'New' }
+      ]
+
+      expect(onlyMajorAge(persons)).to.eql([persons[0]])
+    })
+  })
+})
