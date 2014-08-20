6babcf802542188bd1b15cc8bd99de6706603aa6
Guilherme Nagatomo <gnagotomo@sp.r7.com>
2014-08-20 16:31:55 -0300

    finished moving to expect.js

--- tests/operations.specs.js
+++ tests/operations.specs.js
@@ -1,87 +1,87 @@
 var _ = require('../')
-  , assert = require('assert')
+  , expect = require('expect.js')
 
 describe('Operations', function () {
   describe('#sum()', function () {
     it('should sum properly', function () {
-      assert.equal(_.sum(1, 2), 3)
+      expect(_.sum(1, 2)).to.be(3)
     })
   })
 
   describe('#sumAll()', function () {
     it('should sum all the items from the list', function () {
-      assert.equal(_.sumAll([1, 2, 3]), 6)
+      expect(_.sumAll([1, 2, 3])).to.be(6)
     })
 
     it('should skip non numbers', function () {
-      assert.equal(_.sumAll(['1', 1, undefined, 2, 3]), 6)
+      expect(_.sumAll(['1', 1, undefined, 2, 3])).to.be(6)
     })
   })
 
   describe('#inc()', function () {
     it('should add one', function () {
       var a = 1
-      assert.equal(a, 1)
+      expect(a).to.be(1)
 
       a = _.inc(a)
-      assert.equal(a, 2)
+      expect(a).to.be(2)
     })
   })
 
   describe('#subtract()', function () {
     it('should subtract numbers', function () {
-      assert.equal(_.subtract(2, 1), 1)
+      expect(_.subtract(2, 1)).to.be(1)
     })
   })
 
   describe('#dec()', function () {
     it('should subtract one', function () {
-      assert.equal(_.dec(5), 4)
-      assert.equal(_.dec(5.5), 4.5)
+      expect(_.dec(5)).to.be(4)
+      expect(_.dec(5.5)).to.be(4.5)
     })
   })
 
   describe('#multiply()', function () {
     it('should multiply two numbers', function () {
-      assert.equal(_.multiply(2, 3), 6)
+      expect(_.multiply(2, 3)).to.be(6)
     })
   })
 
   describe('#divide()', function () {
     it('should divide', function () {
-      assert.equal(_.divide(4, 2), 2)
-      assert.equal(_.divide(5, 2), 2.5)
+      expect(_.divide(4, 2)).to.be(2)
+      expect(_.divide(5, 2)).to.be(2.5)
     })
   })
 
   describe('#double()', function () {
     it('should return the double of the number', function () {
-      assert.equal(_.double(2), 4)
-      assert.equal(_.double(5), 10)
-      assert.equal(_.double(2.5), 5)
-      assert.equal(_.double(3.3), 6.6)
+      expect(_.double(2)).to.be(4)
+      expect(_.double(5)).to.be(10)
+      expect(_.double(2.5)).to.be(5)
+      expect(_.double(3.3)).to.be(6.6)
     })
   })
 
   describe('#max()', function () {
     it('should return the largest number from the list', function () {
-      assert.equal(_.max([1, 2]), 2)
-      assert.equal(_.max([-1, 0]), 0)
+      expect(_.max([1, 2])).to.be(2)
+      expect(_.max([-1, 0])).to.be(0)
     })
 
     it('should return -Infinity if the list is empty', function () {
-      assert.equal(_.max([]), -Infinity)
+      expect(_.max([])).to.be(-Infinity)
     })
   })
 
   describe('#min()', function () {
     it('should return the smallest number from the list', function () {
-      assert.equal(_.min([1, 2]), 1)
-      assert.equal(_.min([-1, 0]), -1)
+      expect(_.min([1, 2])).to.be(1)
+      expect(_.min([-1, 0])).to.be(-1)
     })
 
     it('should return Infinity if the list is empty', function () {
-      assert.equal(_.min([]), Infinity)
+      expect(_.min([])).to.be(Infinity)
     })
   })
 })
