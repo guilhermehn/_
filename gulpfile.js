e61890675bd9ce21a1192ae8f648076a0b2cd711
Guilherme Nagatomo <gnagotomo@sp.r7.com>
2014-11-10 15:41:56 -0200

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
-var files = ['index.js', 'lib/*.js', 'tests/*.js'];
+var files = ['index.js', 'fundash.js', 'tests/*.js'];
 
 gulp.task('default', function () {
   return gulp.src(files)
